cfc0b426174e199c66664a7694a7df4aef5f88c8
qidan521 <3153123611@qq.com>
2019-07-17 17:22:11 +0800

    20190717_版本1

---
+++ src/multithread/SendThread.java
@@ -0,0 +1,28 @@
+package multithread;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SendThread extends Thread{
+	private Socket s;
+    public SendThread(Socket s){
+        this.s = s;
+    }
+    public void run(){
+        try {
+            OutputStream os = s.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            while(true){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("«Î ‰»Î£∫");
+                String str = sc.next();
+                dos.writeUTF(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
